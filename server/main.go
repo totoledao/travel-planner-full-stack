40784b100db332945c7305660ff043476a7e249c
Guilherme Toledo <totoledao@hotmail.com>
2024-07-15 19:19:12 -0300

    Create server and basic routing

--- server/main.go
+++ server/main.go
@@ -1,5 +1,113 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"server/internal/api"
+	"server/internal/api/spec"
+	"syscall"
+	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	err := run(ctx)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Closing...")
+}
+
+func run(ctx context.Context) error {
+	conf := zap.NewDevelopmentConfig()
+	conf.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	logger, err := conf.Build()
+	if err != nil {
+		return err
+	}
+	logger = logger.Named("travel_planner")
+	defer logger.Sync()
+
+	err = godotenv.Load()
+	if err != nil {
+		return err
+	}
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	))
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		return err
+	}
+
+	si := api.NewAPI(pool, logger)
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+	r.Mount("/", spec.Handler(&si))
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	defer func() {
+		const timeout = 30 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			logger.Error("Failed to shutdown server", zap.Error(err))
+		}
+	}()
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-errChan:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+	}
+
+	return nil
 }
